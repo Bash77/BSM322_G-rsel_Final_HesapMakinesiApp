ab179c95545017a1b2ac0a2cb39647a8c0585ac1
Bashir Mohamed Ali <72501356+Bash77@users.noreply.github.com>
2025-06-05 22:04:45 +0300

    Update BilimselSayfa.xaml.cs

--- BilimselSayfa.xaml.cs
+++ BilimselSayfa.xaml.cs
@@ -11,26 +11,26 @@ namespace HesapMakinesiApp
             InitializeComponent();
         }
 
-        // Giriş temizleme
+      
         private void OnClearClicked(object sender, EventArgs e)
         {
             BilimselEntry.Text = "";
         }
 
-        // Eşittir tuşu (şu an kullanılmıyor, istenirse hesaplama mantığı eklenebilir)
+         
         private void OnEqualClicked(object sender, EventArgs e)
         {
-            // Gerekirse gelecekte işlem eklenecek
+             
         }
 
-        // Karekök
+       
         private async void OnSqrtClicked(object sender, EventArgs e)
         {
             if (await TryParseInput())
                 BilimselEntry.Text = Math.Sqrt(sayi).ToString();
         }
 
-        // Üs alma (xʸ)
+        
         private async void OnPowerClicked(object sender, EventArgs e)
         {
             string giris = await DisplayPromptAsync("Üs İşlemi", "Üssü girin (y):", keyboard: Keyboard.Numeric);
@@ -38,14 +38,14 @@ namespace HesapMakinesiApp
                 BilimselEntry.Text = Math.Pow(sayi, us).ToString();
         }
 
-        // e^x
+       
         private async void OnExpClicked(object sender, EventArgs e)
         {
             if (await TryParseInput())
                 BilimselEntry.Text = Math.Exp(sayi).ToString();
         }
 
-        // ln(x)
+        
         private async void OnLnClicked(object sender, EventArgs e)
         {
             if (await TryParseInput())
@@ -57,7 +57,7 @@ namespace HesapMakinesiApp
             }
         }
 
-        // log10(x)
+        
         private async void OnLogClicked(object sender, EventArgs e)
         {
             if (await TryParseInput())
@@ -69,40 +69,40 @@ namespace HesapMakinesiApp
             }
         }
 
-        // sin(x)
+      
         private async void OnSinClicked(object sender, EventArgs e)
         {
             if (await TryParseInput())
                 BilimselEntry.Text = Math.Sin(DereceToRadyan(sayi)).ToString();
         }
 
-        // cos(x)
+        
         private async void OnCosClicked(object sender, EventArgs e)
         {
             if (await TryParseInput())
                 BilimselEntry.Text = Math.Cos(DereceToRadyan(sayi)).ToString();
         }
 
-        // tan(x)
+        
         private async void OnTanClicked(object sender, EventArgs e)
         {
             if (await TryParseInput())
                 BilimselEntry.Text = Math.Tan(DereceToRadyan(sayi)).ToString();
         }
 
-        // π butonu
+        
         private void OnPiClicked(object sender, EventArgs e)
         {
             BilimselEntry.Text = Math.PI.ToString();
         }
 
-        // e sabiti
+        
         private void OnEClicked(object sender, EventArgs e)
         {
             BilimselEntry.Text = Math.E.ToString();
         }
 
-        // Sayı kontrolü ve geçersiz girişte uyarı
+        
         private async Task<bool> TryParseInput()
         {
             if (double.TryParse(BilimselEntry.Text, out sayi))
@@ -112,7 +112,7 @@ namespace HesapMakinesiApp
             return false;
         }
 
-        // Derece -> Radyan dönüşümü
+       
         private double DereceToRadyan(double derece)
         {
             return (Math.PI / 180) * derece;
